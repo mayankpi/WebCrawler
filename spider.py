cc5e1ed24d2f66d0fbf91487e3fbc9d96fe7f567
mayankpi <mayankm522@gmail.com>
2017-01-18 00:05:18 +0530

    created main class for the crawler

--- spider.py
+++ spider.py
@@ -8,20 +8,20 @@ class Spider:
 
     project_name = ''
     base_url = ''
-    domain_url = ''
+    domain_name = ''
     queue_file = ''
     crawled_file = ''
     queue = set()
     crawled = set()
 
-    def __init__(self, project_name, base_url, domain_url):
+    def __init__(self, project_name, base_url, domain_name):
         Spider.base_url = base_url
-        Spider.domain_url = domain_url
+        Spider.domain_name = domain_name
         Spider.project_name = project_name
         Spider.queue_file = Spider.project_name + "/queue.txt"
         Spider.crawled_file = Spider.project_name + "/crawled.txt"
         self.boot()
-        self.crawl_page('First spider', Spider.base_url)
+        self.crawl_page('First_spider', Spider.base_url)
 
     @staticmethod
     def boot():
