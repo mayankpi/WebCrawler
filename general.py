f029c83e882aa6d3762638eaa467aca2cd3992f1
mayankpi <mayankm522@gmail.com>
2017-01-17 19:05:10 +0530

    added functions for writing the queued and crawled links

--- general.py
+++ general.py
@@ -1,8 +1,21 @@
 import os
 
-def create_new_project(directory):
+def create_project_dir(directory):
     if not os.path.exists(directory):
         print("creating directory")
         os.makedirs(directory)
 
-create_new_project('mayankwebcrawler')
+#create_project_dir('mayankwebcrawler')
+
+def create_data_file(project_name, base_url):
+    queue = project_name + "queue.txt"
+    crawled = project_name + "crawled.txt"
+    if not os.path.isfile(queue):
+        write_file(queue,base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
