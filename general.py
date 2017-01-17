5f2319c3d718aeffb60f0aee60c3372299b15a7e
mayankpi <mayankm522@gmail.com>
2017-01-17 19:16:41 +0530

    created functions for adding and deleting data(urls)

--- general.py
+++ general.py
@@ -7,6 +7,8 @@ def create_project_dir(directory):
 
 #create_project_dir('mayankwebcrawler')
 
+
+#creating queued and crawled files
 def create_data_file(project_name, base_url):
     queue = project_name + "/queue.txt"
     crawled = project_name + "/scrawled.txt"
@@ -15,7 +17,18 @@ def create_data_file(project_name, base_url):
     if not os.path.isfile(crawled):
         write_file(crawled, '')
 
+#creating new file
 def write_file(path, data):
     f = open(path, 'w')
     f.write(data)
     f.close()
+
+#add more urls to the existing file
+def append_to_file(path, data):
+    with open(path, 'a') as file:
+        file.write(data,"\n")
+
+#delete the contents of the given file
+def delete_file_content(path):
+    with open(path, 'w'):
+        pass
